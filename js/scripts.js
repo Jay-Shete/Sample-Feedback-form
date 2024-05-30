71357d1f94695f9ef660972d31844e0c076ac031
Jay Shete <73311611+Jay-Shete@users.noreply.github.com>
2024-05-30 15:03:27 -0700

    Update and rename script.js to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+    // Here you can add your form submission logic
+});
