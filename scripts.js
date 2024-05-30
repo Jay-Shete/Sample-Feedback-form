cb2e3b83dc158ddc0303b0216b46ea59acfe5b11
Jay Shete <73311611+Jay-Shete@users.noreply.github.com>
2024-05-30 15:05:45 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('feedbackForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted successfully!');
-    // Here you can add your form submission logic
-});
